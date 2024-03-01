8f9b4a952e5131720c75d3f54248973d19d24fff
Priscilla Son <priscilla.lee.son@gmail.com>
2024-02-29 18:58:55 -0800

    Build out tile component

--- src/components/Tile.tsx
+++ src/components/Tile.tsx
@@ -1,7 +1,27 @@
-export const Tile = () => {
+export const Tile: React.FC = () => {
   return (
-    <div>
-      <h1>Tile</h1>
+    <div
+      className="flex-col mr-[16px] w-[400px] h-[500px] rounded-lg border-pink border
+    "
+    >
+      <img
+        data-hint="product-image"
+        className=" w-full h-5/6 rounded-t-lg"
+        src="https://kyliecosmetics.com/cdn/shop/files/KJC_LLPRP_23_ComesNaturally_Stylized_800x.jpg?v=1701816854"
+        alt="product-image"
+      />
+      <div
+        data-hint="product-details"
+        className="bg-white w-full px-4 py-3 h-1/6 text-lg rounded-lg"
+      >
+        <div>
+          <div className="flex justify-between">
+            <div className="font-bold">comes naturally</div>
+            <div>$17</div>
+          </div>
+          <div>precious pout lip liner</div>
+        </div>
+      </div>
     </div>
   );
 };
