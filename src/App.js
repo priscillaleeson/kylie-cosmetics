58f0de8614772089ea85f27861736be9a9b2de2d
Priscilla Son <priscilla.lee.son@gmail.com>
2024-02-29 18:59:03 -0800

    Add components to App

--- src/App.js
+++ src/App.js
@@ -1,7 +1,16 @@
-import "./App.css";
+import { Tile } from "./components/Tile";
+import { SwiperContainer } from "./components/Swiper-Container";
 
 function App() {
-  return <div className="flex justify-center text-3xl">hello</div>;
+  return (
+    <div className="flex justify-center text-3xl m-40 text-chocolate">
+      <SwiperContainer>
+        <Tile />
+        <Tile />
+        <Tile />
+      </SwiperContainer>
+    </div>
+  );
 }
 
 export default App;
