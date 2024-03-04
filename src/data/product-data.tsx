94145e3788964f5c4d873f5cd7a22b27c6d0db27
Priscilla Son <priscilla.lee.son@gmail.com>
2024-03-04 14:25:55 -0800

    Add properties and types to product data

--- src/data/product-data.tsx
+++ src/data/product-data.tsx
@@ -1,8 +1,23 @@
-export const productData = [
+interface ProductDataProps {
+  id: number;
+  name: string;
+  price: string;
+  variations: string[];
+  avgRating: number;
+  numberOfRatings: number;
+  imageCoverSrc: string;
+  secondImageSrc: string;
+}
+
+export const productData: ProductDataProps[] = [
   {
     id: 1,
     name: "precious pout lip liner",
     price: "$17",
     variations: ["cocoa", "smitten", "comes naturally"],
+    avgRating: 5,
+    numberOfRatings: 85,
+    imageCoverSrc: "placeHolderString",
+    secondImageSrc: "placeHolderString",
   },
 ];
