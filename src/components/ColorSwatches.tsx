f37459034a502982bfc533c27488e10a360290fc
Priscilla Son <priscilla.lee.son@gmail.com>
2024-03-04 18:00:27 -0800

    Create color swatches component

---
+++ src/components/ColorSwatches.tsx
@@ -0,0 +1,24 @@
+export const placeholderColorArray = ["#A95952", "#9C595A", "#9D5546"];
+
+interface ColorSwatchesProps {
+  colors: string[];
+}
+
+export const ColorSwatches: React.FC<ColorSwatchesProps> = ({ colors }) => {
+  let colorSwatchArray;
+
+  if (colors.length > 0) {
+    return (colorSwatchArray = colors.map((color) => {
+      return (
+        <div
+          className="rounded-full border-black h-4 w-4 mr-1"
+          style={{
+            backgroundColor: color,
+          }}
+        ></div>
+      );
+    }));
+  }
+
+  return <div>{colorSwatchArray}</div>;
+};
