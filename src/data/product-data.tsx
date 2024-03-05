0cc29aaae5e93f64f706e1fad371c46721dbe23d
Priscilla Son <priscilla.lee.son@gmail.com>
2024-03-04 17:58:27 -0800

    Add properties to productData array

--- src/data/product-data.tsx
+++ src/data/product-data.tsx
@@ -2,11 +2,18 @@ interface ProductDataProps {
   id: number;
   name: string;
   price: string;
-  variations: string[];
+  colorVariations: {
+    name: string;
+    hexCode: string;
+    featuredImage: string;
+    alternateImage: string;
+  }[];
   avgRating: number;
   numberOfRatings: number;
-  imageCoverSrc: string;
-  secondImageSrc: string;
+  featuredImageSrc: string;
+  featuredImageSrcAlt: string;
+  alternateImageSrc: string;
+  alternateImageSrcAlt: string;
 }
 
 export const productData: ProductDataProps[] = [
@@ -14,10 +21,32 @@ export const productData: ProductDataProps[] = [
     id: 1,
     name: "precious pout lip liner",
     price: "$17",
-    variations: ["cocoa", "smitten", "comes naturally"],
+    colorVariations: [
+      {
+        name: "cocoa",
+        hexCode: "#A95952",
+        featuredImage: "placeholder",
+        alternateImage: "placeholder",
+      },
+      {
+        name: "smitten",
+        hexCode: "#9C595A",
+        featuredImage: "placeholder",
+        alternateImage: "placeholder",
+      },
+      {
+        name: "comes naturally",
+        hexCode: "#9D5546",
+        featuredImage: "placeholder",
+        alternateImage: "placeholder",
+      },
+    ],
     avgRating: 5,
     numberOfRatings: 85,
-    imageCoverSrc: "placeHolderString",
-    secondImageSrc: "placeHolderString",
+    featuredImageSrc:
+      "https://kyliecosmetics.com/cdn/shop/files/KJC_LLPRP_23_ComesNaturally_Stylized_800x.jpg?v=1701816854",
+    featuredImageSrcAlt: "placeHolderString",
+    alternateImageSrc: "placeHolderString",
+    alternateImageSrcAlt: "placeHolderString",
   },
 ];
