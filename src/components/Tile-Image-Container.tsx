38f25590e3913158b027c4b6a525f228ec094895
Priscilla Son <priscilla.lee.son@gmail.com>
2024-03-06 20:32:45 -0800

    Pass imgSrc props to TileImageContainer

--- src/components/Tile-Image-Container.tsx
+++ src/components/Tile-Image-Container.tsx
@@ -1,27 +1,29 @@
-const placeholderImages = [
-  {
-    src1: "https://kyliecosmetics.com/cdn/shop/files/KJC_LLPRP_23_ComesNaturally_Stylized_800x.jpg?v=1701816854",
-    src2: "https://kyliecosmetics.com/cdn/shop/files/KJC_LLPRP_23_ComesNaturally_Swatch_800x.jpg?v=1701816844",
-  },
-  {
-    src1: "https://kyliecosmetics.com/cdn/shop/files/KJC_LLPRP_23_ComesNaturally_Swatch_800x.jpg?v=1701816844",
-    src2: "https://kyliecosmetics.com/cdn/shop/files/KJC_LLPRP_23_ComesNaturally_Stylized_800x.jpg?v=1701816854",
-  },
-];
-
-interface TileImageContainerProps {
-  thumbnailPhoto: { src: string };
-}
-export const TileImageContainer: React.FC<TileImageContainerProps> = ({
-  thumbnailPhoto,
+export const TileImageContainer = ({
+  showProductDetailsMenu,
+  imgSrc,
+  imgOverlaySrc,
+}: {
+  showProductDetailsMenu: boolean;
+  imgSrc: string;
+  imgOverlaySrc: string;
 }) => {
   return (
     <div>
-      <div data-hint="image-container" className="h-4/6">
+      <div data-hint="image-container" className="h-4/6 relative">
         <img
           data-hint="product-image"
-          className="object-cover w-full h-full rounded-t-lg"
-          src={thumbnailPhoto.src}
+          className="relative object-cover w-full h-full rounded-t-lg"
+          src={imgSrc}
+          alt="product-placeholder"
+        />
+        <img
+          style={{
+            opacity: showProductDetailsMenu ? "1" : "0",
+            transition: "opacity 0.2s",
+          }}
+          data-hint="product-image-overlay absolute"
+          className="absolute top-0 object-cover w-full h-full rounded-t-lg"
+          src={imgOverlaySrc}
           alt="product-placeholder"
         />
       </div>
