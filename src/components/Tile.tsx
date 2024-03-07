7ebcd692e0ff7ca7b1c34f88ce08a6801485018c
Priscilla Son <priscilla.lee.son@gmail.com>
2024-03-06 20:33:04 -0800

    Refactor

--- src/components/Tile.tsx
+++ src/components/Tile.tsx
@@ -1,39 +1,33 @@
-import { StarRating } from "./Star";
+import { StarRating } from "./StarRating";
 import { ColorSwatches } from "./ColorSwatches";
 import { placeholderColorArray } from "./ColorSwatches";
 import { useState } from "react";
 import { TileImageContainer } from "./Tile-Image-Container";
 import { productData } from "../data/product-data";
-import {
-  AddToCartButton,
-  QuantitySelector,
-  CartContainer,
-} from "./CartFeatures";
+import { CartContainer } from "./CartFeatures";
+import { ProductDetailsContainer } from "./ProductDetailsContainer";
+import { ProductDetails } from "./ProductDetails";
 
-//just need to switch out initial photo and alt photo with productData
-//currently these are placeholder images
-const initialPhoto = {
-  src: "https://kyliecosmetics.com/cdn/shop/files/KJC_LLPRP_23_ComesNaturally_Stylized_800x.jpg?v=1701816854",
-};
-
-const altPhoto = {
-  src: "https://kyliecosmetics.com/cdn/shop/files/KJC_LLPRP_23_ComesNaturally_Swatch_800x.jpg?v=1701816844",
-};
+const placeholderImages = [
+  {
+    src1: "https://kyliecosmetics.com/cdn/shop/files/KJC_LLPRP_23_ComesNaturally_Stylized_800x.jpg?v=1701816854",
+    src2: "https://kyliecosmetics.com/cdn/shop/files/KJC_LLPRP_23_ComesNaturally_Swatch_800x.jpg?v=1701816844",
+  },
+  {
+    src1: "https://kyliecosmetics.com/cdn/shop/files/KJC_LLPRP_23_ComesNaturally_Swatch_800x.jpg?v=1701816844",
+    src2: "https://kyliecosmetics.com/cdn/shop/files/KJC_LLPRP_23_ComesNaturally_Stylized_800x.jpg?v=1701816854",
+  },
+];
 
 export const Tile: React.FC = () => {
-  const [thumbnailPhoto, setThumbnailPhoto] = useState(initialPhoto);
+  const [showProductDetailsMenu, setShowProductDetailsMenu] = useState(false);
 
   const handleMouseEnter = () => {
-    setThumbnailPhoto(altPhoto);
+    setShowProductDetailsMenu(true);
   };
 
   const handleMouseLeave = () => {
-    setThumbnailPhoto(initialPhoto);
-  };
-
-  const handleHover = () => {
-    //use this function to hide and show color swatches and cart features
-    
+    setShowProductDetailsMenu(false);
   };
 
   return (
@@ -42,27 +36,23 @@ export const Tile: React.FC = () => {
       onMouseLeave={handleMouseLeave}
       className="mb-5 lg:mb-0 flex-col mr-[16px] min-w-[200px] max-w-[400px] rounded-lg border-pink border"
     >
-      <TileImageContainer thumbnailPhoto={thumbnailPhoto} />
-      <div
-        data-hint="product-details"
-        className="bg-white w-full px-3 py-3 h-2/6 text-sm rounded-lg"
-      >
-        <div data-hint="color-swatch">
-          <ColorSwatches colors={placeholderColorArray} />
-        </div>
-        <div>
-          <div>
-            {/* input rating from provided data here */}
-            <StarRating avgRating={5} numberOfRatings={85} />
-          </div>
-          <div className="flex justify-between font-bold">
-            <div>comes naturally</div>
-            <div>$17</div>
-          </div>
-          <div>precision pout lip liner</div>
-          <CartContainer />
-        </div>
-      </div>
+      <TileImageContainer
+        showProductDetailsMenu={showProductDetailsMenu}
+        imgSrc={placeholderImages[0].src1}
+        imgOverlaySrc={placeholderImages[0].src2}
+      />
+
+      <ProductDetailsContainer>
+        <ColorSwatches colors={placeholderColorArray} />
+        {/* input rating from provided data here */}
+        <StarRating avgRating={4.5} numberOfRatings={85} />
+        <ProductDetails
+          name="precision pout liner"
+          price={17}
+          colorVariation="comes naturally"
+        />
+        <CartContainer price={17} />
+      </ProductDetailsContainer>
     </div>
   );
 };
