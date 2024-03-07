77444bc22cd874f4c95fb0974f6184a2419b2369
Priscilla Son <priscilla.lee.son@gmail.com>
2024-03-07 13:57:52 -0800

    Switch out each tile with rendered tiles

--- src/App.js
+++ src/App.js
@@ -1,16 +1,12 @@
 import { Tile } from "./components/Tile";
 import { SwiperContainer } from "./components/Swiper-Container";
 import { AddToCartButton } from "./components/CartFeatures";
+import { renderedTiles } from "./components/Tile";
 
 function App() {
   return (
     <div className="flex justify-center text-3xl items-center m-16 text-chocolate">
-      <SwiperContainer>
-        <Tile />
-        <Tile />
-        <Tile />
-        <Tile />
-      </SwiperContainer>
+      <SwiperContainer>{renderedTiles}</SwiperContainer>
     </div>
   );
 }
