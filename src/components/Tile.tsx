6c096fd22e009c07c75a359de766e7279261ec8c
Priscilla Son <priscilla.lee.son@gmail.com>
2024-03-12 11:45:11 -0700

    Create smoother detail menu animation

--- src/components/Tile.tsx
+++ src/components/Tile.tsx
@@ -47,25 +47,35 @@ export const Tile: React.FC<TileProps> = ({
     <div
       onMouseEnter={handleMouseEnter}
       onMouseLeave={handleMouseLeave}
-      className="mb-5 lg:mb-0 flex-col mr-[16px] min-w-[200px] max-w-[400px] rounded-lg border-pink border"
+      className="mb-5 lg:mb-0 flex-col mr-[16px] min-w-[200px] max-w-[365px] max-h-[500px] rounded-lg border-pink border relative overflow-hidden group"
     >
       <TileImageWrapper
-        showProductDetailsMenu={showProductDetailsMenu}
         imgSrc={imgSrc}
         imgOverlaySrc={altImgSrc}
         tag={tag}
+        showProductDetailsMenu={showProductDetailsMenu}
       />
 
       <ProductDetailsContainer>
-        <ColorSwatches colors={colorVariations} />
-        {/* input rating from provided data here */}
-        <StarRating avgRating={avgRating} numberOfRatings={numberOfRatings} />
-        <ProductDetails
-          name={name}
-          price={price}
-          colorVariations={colorVariations}
-        />
-        <CartWrapper price={price} />
+        <div className="bg-white absolute bottom-0 left-0 right-0 p-3 pt-2">
+          <div className="pt-2 overflow-hidden transition-all ease-linear max-h-0 group-hover:max-h-64 group-hover:duration-[1100ms]">
+            <ColorSwatches colors={colorVariations} />
+          </div>
+          {/* input rating from provided data here */}
+          <StarRating avgRating={avgRating} numberOfRatings={numberOfRatings} />
+          <ProductDetails
+            name={name}
+            price={price}
+            colorVariations={colorVariations}
+          />
+
+          <div className="relative transition-all ease-in-out  overflow-hidden max-h-0 group-hover:max-h-64 group-hover:duration-[700ms]">
+            {/*  overflow-hidden max-h-0 group-hover:max-h-64 */}
+            <div className="group-hover:translate-y-0 translate-y-20 transition-all duration-200">
+              <CartWrapper price={price} />
+            </div>
+          </div>
+        </div>
       </ProductDetailsContainer>
     </div>
   );
