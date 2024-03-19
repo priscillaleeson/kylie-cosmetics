f593ee2ca8fe0dd3b88c99288261d4c3e3bc6499
Priscilla Son <priscilla.lee.son@gmail.com>
2024-03-18 22:26:47 -0700

    Create LogoWrapper

---
+++ src/components/LogoWrapper.tsx
@@ -0,0 +1,5 @@
+import { logo } from "../images/kylieCosmeticsLogo";
+
+export const LogoWrapper: React.FC = () => {
+  return <div className="flex justify-center p-5 text-chocolate ">{logo}</div>;
+};
