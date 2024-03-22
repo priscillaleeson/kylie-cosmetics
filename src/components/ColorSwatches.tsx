fe2afcb93ef12892add5f79ea463572c94723a5f
Priscilla Son <priscilla.lee.son@gmail.com>
2024-03-21 22:54:55 -0700

    Adjust color swatches for sliding menu animation

--- src/components/ColorSwatches.tsx
+++ src/components/ColorSwatches.tsx
@@ -37,22 +37,29 @@ export const ColorSwatches: React.FC<ColorSwatchesProps> = ({
       {hasColors &&
         colors.map((color) => {
           return (
-            <div className={classNames({ "my-1 flex": isVertical })}>
+            <div
+              className={classNames({
+                "my-2 flex relative": isVertical,
+              })}
+            >
               {/* <div className={isVertical ? "my-1 flex" : "m-0"}> */}
               <button
                 id={color.name}
                 className={classNames(
                   "rounded-full",
                   "focus:ring-1  focus:ring-black",
+
                   isVertical
-                    ? "h-7 w-7 mr-2 mb-1 focus:ring-offset-[1.8px]"
+                    ? "h-7 w-7 mr-2 mb-1 focus:ring-offset-[1.8px] absolute top-0 left-0"
                     : "h-5 w-5 mr-1 focus:ring-offset-[.7px]"
                 )}
                 style={{
                   backgroundColor: color.hexCode,
                 }}
               ></button>
-              {isVertical && <div className="font-medium">{color.name}</div>}
+              {isVertical && (
+                <div className="font-medium ml-9">{color.name}</div>
+              )}
             </div>
           );
         })}
