9bde4d88722817dcdccd2a58d8da33d713ab2cf5
Priscilla Son <priscilla.lee.son@gmail.com>
2024-03-28 16:39:53 -0700

    Map to data and reformat CSS

--- src/components/Tile-Image-Wrapper.tsx
+++ src/components/Tile-Image-Wrapper.tsx
@@ -1,36 +1,55 @@
 import { OptionalTag } from "./OptionalTag";
+import classNames from "classnames";
 
 export const TileImageWrapper = ({
-  imgSrc,
-  imgOverlaySrc,
+  selectedColorOption,
+  toggleProductImage,
   tag,
   showProductDetailsMenu,
 }: {
-  imgSrc: string;
-  imgOverlaySrc: string;
+  selectedColorOption: {
+    productName: string;
+    colorName: string;
+    featuredImage: string;
+    secondaryImage: string;
+  };
   tag?: string;
+  toggleProductImage: boolean;
   showProductDetailsMenu: boolean;
 }) => {
   return (
     <div>
       <div data-hint="image-container" className=" relative pb-16">
         {tag && <OptionalTag tagText={tag} />}
+
         <img
           data-hint="product-image"
-          className="relative object-cover w-full h-full rounded-t-lg"
-          src={imgSrc}
-          alt="product-placeholder"
+          className={classNames(
+            "relative",
+            "object-cover w-full h-full",
+            "rounded-t-lg"
+          )}
+          src={selectedColorOption.featuredImage}
+          alt={selectedColorOption.productName}
         />
-        <img
-          data-hint="product-image-overlay absolute"
-          className="absolute top-0 object-cover w-full h-full rounded-t-lg"
-          src={imgOverlaySrc}
-          alt="product-placeholder"
-          style={{
-            opacity: showProductDetailsMenu ? "1" : "0",
-            transition: "opacity 0.2s",
-          }}
-        />
+
+        {toggleProductImage && (
+          <img
+            data-hint="product-image-overlay absolute"
+            className={classNames(
+              "absolute top-0 left-0 right-0",
+              "object-cover",
+              "w-full h-full",
+              "rounded-t-lg"
+            )}
+            src={selectedColorOption.secondaryImage}
+            alt={selectedColorOption.productName}
+            style={{
+              opacity: showProductDetailsMenu ? "1" : "0",
+              transition: "opacity 0.2s",
+            }}
+          />
+        )}
       </div>
     </div>
   );
