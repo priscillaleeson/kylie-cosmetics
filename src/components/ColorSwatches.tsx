ca45b6e09b325ce4ac3af045671bb49ca7705218
Priscilla Son <priscilla.lee.son@gmail.com>
2024-03-28 16:43:36 -0700

    Add types and map data

--- src/components/ColorSwatches.tsx
+++ src/components/ColorSwatches.tsx
@@ -2,37 +2,56 @@ import { BiPlus } from "react-icons/bi";
 import classNames from "classnames";
 
 interface ColorSwatchesProps {
+  productName?: string;
   isVertical: boolean;
-  showOpacityLayer?: boolean;
-  setShowOpacityLayer: (showOpacityLayer: boolean) => void;
+  showColorPickerSlideMenu?: boolean;
+  setShowColorPickerSlideMenu: (showOpacityLayer: boolean) => void;
   colors: {
-    name?: string;
-    hexCode?: string;
-    featuredImage?: string;
-    secondaryImage?: string;
+    name: string;
+    hexCode: string;
+    featuredImage: string;
+    secondaryImage: string;
   }[];
+  selectedColorOption?: {
+    productName: string;
+    colorName: string;
+    featuredImage: string;
+    secondaryImage: string;
+  };
+  setSelectedColorOption?: ({
+    productName,
+    colorName,
+    featuredImage,
+    secondaryImage,
+  }: {
+    productName: string;
+    colorName: string;
+    featuredImage: string;
+    secondaryImage: string;
+  }) => void;
 }
 
 export const ColorSwatches: React.FC<ColorSwatchesProps> = ({
   colors,
+  productName,
   isVertical,
-  showOpacityLayer,
-  setShowOpacityLayer,
+  showColorPickerSlideMenu,
+  setShowColorPickerSlideMenu,
+  selectedColorOption,
+  setSelectedColorOption,
 }) => {
   const hasColors = colors.length > 0;
 
-  const handleSeeMoreColorsClick = () => {
+  const handleOpenColorPickerClick = () => {
     console.log("I clicked!");
-    setShowOpacityLayer(true);
+    setShowColorPickerSlideMenu(true);
   };
 
   return (
     <div
-      className={classNames(
-        "flex relative top-0 pb-1",
-        { "flex-col": isVertical }
-        // isVertical ? "flex-col" : "flex-row"
-      )}
+      className={classNames("flex relative top-0 pb-1 mt-4", {
+        "flex-col ml-2": isVertical,
+      })}
     >
       {hasColors &&
         colors.map((color) => {
@@ -42,12 +61,27 @@ export const ColorSwatches: React.FC<ColorSwatchesProps> = ({
                 "my-2 flex relative": isVertical,
               })}
             >
-              {/* <div className={isVertical ? "my-1 flex" : "m-0"}> */}
               <button
+                onClick={() => {
+                  console.log("i want to change the image here");
+                  if (setSelectedColorOption) {
+                    setSelectedColorOption({
+                      productName: productName ?? "",
+                      colorName: color.name,
+                      featuredImage: color.featuredImage,
+                      secondaryImage: color.secondaryImage,
+                    });
+                  }
+
+                  console.log(
+                    `selectedColorOption: ${selectedColorOption?.featuredImage}`
+                  );
+                }}
                 id={color.name}
                 className={classNames(
                   "rounded-full",
                   "focus:ring-1  focus:ring-black",
+                 
 
                   isVertical
                     ? "h-7 w-7 mr-2 mb-1 focus:ring-offset-[1.8px] absolute top-0 left-0"
@@ -58,7 +92,7 @@ export const ColorSwatches: React.FC<ColorSwatchesProps> = ({
                 }}
               ></button>
               {isVertical && (
-                <div className="font-medium ml-9">{color.name}</div>
+                <div className="font-medium text-lg ml-9">{color.name}</div>
               )}
             </div>
           );
@@ -72,8 +106,8 @@ export const ColorSwatches: React.FC<ColorSwatchesProps> = ({
               "border rounded-full border-black"
             )}
           >
-            <div className="p-2">
-              <BiPlus onClick={handleSeeMoreColorsClick} />
+            <div className="p-2" onClick={handleOpenColorPickerClick}>
+              <BiPlus />
             </div>
           </button>
         )}
@@ -81,22 +115,3 @@ export const ColorSwatches: React.FC<ColorSwatchesProps> = ({
     </div>
   );
 };
-
-// function SeeMoreColorsButton() {
-//   const handleSeeMoreColorsClick = () => {
-//     console.log("I clicked!");
-//   };
-//   return (
-//     <button
-//       className={classNames(
-//         "flex justify-center items-center",
-//         "h-5 w-5 mr-1",
-//         "border rounded-full border-black"
-//       )}
-//     >
-//       <div className="p-2" onClick={handleSeeMoreColorsClick}>
-//         <BiPlus />
-//       </div>
-//     </button>
-//   );
-//
