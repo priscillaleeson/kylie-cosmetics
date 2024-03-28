5ace79a8a4ef439cf4fe038165bb6b46874d8d06
Priscilla Son <priscilla.lee.son@gmail.com>
2024-03-28 16:48:08 -0700

    Add bottom-0 to absolute position

--- src/components/OpacityLayer.tsx
+++ src/components/OpacityLayer.tsx
@@ -8,7 +8,7 @@ export const OpacityLayer = ({
     <div
       data-hint="opacity-layer"
       className={classNames(
-        " absolute right-0 left-0 top-0 bg-white z-50 h-screen",
+        "absolute right-0 left-0 bottom-0 top-0 bg-white z-50 h-screen",
         showColorPickerSlideMenu
           ? "opacity-60"
           : "opacity-0 pointer-events-none"
